40d6afcbcbd7fe5600493f261d82796a8987a643
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2017-04-17 21:53:37 +0200

    Add webconfig, main controller and some folder replecement.

---
+++ src/main/java/com/ninjabooks/configuration/AppConfig.java
@@ -0,0 +1,39 @@
+package com.ninjabooks.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * @author Piotr 'pitrecki' Nowak
+ * @since 1.0
+ */
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.ninjabooks")
+public class AppConfig extends WebMvcConfigurerAdapter
+{
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+  }
+
+  @Bean
+  public ViewResolver getViewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setPrefix("/WEB-INF/static/");
+    viewResolver.setSuffix(".html");
+    return viewResolver;
+  }
+
+  @Override
+  public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    configurer.enable();
+  }
+}
---
+++ src/main/java/com/ninjabooks/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.ninjabooks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Main controller is responsible to handle request with <b>'/'</b>
+ * and render the request to index.html
+ *
+ * @author Piotr 'pitrecki' Nowak
+ * @since 1.0
+ */
+@Controller
+public class MainController
+{
+  @RequestMapping(value = "/", method = RequestMethod.GET)
+  public String index() {
+    return "index";
+  }
+}
