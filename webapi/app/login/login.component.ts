92ff81833b51e8b667e20b091794fadb85577c75
Karol Kawski <karol.kawski@protonmail.com>
2017-04-17 10:18:42 +0200

    remove e2e, replace files, change directory

---
+++ webapi/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Http } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+
+import { HttpService } from '../services/index';
+
+@Component({
+  moduleId: module.id,
+  templateUrl: `login.component.html`,
+})
+export class LoginComponent  {
+
+
+    }
