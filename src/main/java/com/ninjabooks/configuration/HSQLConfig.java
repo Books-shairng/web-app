76f4beca582ea52c592b5fa554102c9722b576b8
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2017-04-30 19:13:22 +0200

    Fix problem with dao layer test, when profile set to test. Add dummy sql data, which will be inserted when dev profile is active.

--- src/main/java/com/ninjabooks/configuration/HSQLConfig.java
+++ src/main/java/com/ninjabooks/configuration/HSQLConfig.java
@@ -1,7 +1,10 @@
 package com.ninjabooks.configuration;
 
 import org.hibernate.SessionFactory;
+import org.hsqldb.util.DatabaseManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
@@ -31,16 +34,22 @@ import java.util.Properties;
 @Profile(value = {"dev", "test"})
 public class HSQLConfig implements DBConnectConfig
 {
+    @Autowired
+    private Environment environment;
+
     @Bean
     @Override
     public SessionFactory sessionFactory() {
         LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource());
-        String activeProfile = System.getProperty("spring.profiles.active");
+        String activeProfile =  environment.getActiveProfiles()[0];
+
         builder.scanPackages("com.ninjabooks.domain")
             .addProperties(hibernateProperties());
 
-        if (activeProfile.equals("dev"))
+        if (activeProfile.equals("dev") && !activeProfile.isEmpty()) {
             builder.addProperties(importDataToDB());
+            DatabaseManager.threadedDBM();
+        }
 
         return builder.buildSessionFactory();
     }
@@ -50,7 +59,7 @@ public class HSQLConfig implements DBConnectConfig
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
-        dataSource.setUrl("jdbc:hsqldb:mem:butterfly");
+        dataSource.setUrl("jdbc:hsqldb:mem:books-sharing;ifexists=false");
         dataSource.setUsername("root");
         dataSource.setPassword("");
         return dataSource;
@@ -63,7 +72,7 @@ public class HSQLConfig implements DBConnectConfig
         properties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.format_sql", "true");
-        properties.put("hibernate.hbm2ddl.auto", "update");
+        properties.put("hibernate.hbm2ddl.auto", "create");
 //        properties.put("hibernate.current_session_context_class", "thread");
         properties.put("hibernate.rollback", "false");
         return properties;
@@ -73,7 +82,7 @@ public class HSQLConfig implements DBConnectConfig
     @Profile(value = "dev")
     public Properties importDataToDB() {
         Properties properties = new Properties();
-        properties.put("hibernate.hbm2ddl.import_files", "");
+        properties.put("hibernate.hbm2ddl.import_files", "queries/import.sql");
         return properties;
     }
 
