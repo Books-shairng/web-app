9d3fd7c5717e4ef5ec017c99df41c5edb3753e6d
Karol Kawski <karol.kawski@protonmail.com>
2017-06-24 19:18:07 +0200

    logout, book model wireframe, routing

--- src/main/webapp/resources/app/app.routes.js
+++ src/main/webapp/resources/app/app.routes.js
@@ -5,11 +5,19 @@ var registration_component_1 = require("./registration/registration.component");
 var search_component_1 = require("./search/search.component");
 var index_1 = require("./notifications/index");
 var index_2 = require("./guards/index");
+var index_3 = require("./addbook/index");
+var index_4 = require("./bookinfo/index");
+var index_5 = require("./bookslist/index");
+var index_6 = require("./settings/index");
 var router = [
     { path: '', component: index_1.NotificationComponent, canActivate: [index_2.AuthGuard] },
     { path: 'login', component: login_component_1.LoginComponent },
     { path: 'registration', component: registration_component_1.RegistrationComponent },
     { path: 'search', component: search_component_1.SearchComponent },
+    { path: 'addbook', component: index_3.AddBookComponent },
+    { path: 'bookinfo', component: index_4.BookInfoComponent },
+    { path: 'bookslist', component: index_5.BooksListComponent },
+    { path: 'settings', component: index_6.SettingsComponent },
     { path: '**', redirectTo: '' },
 ];
 exports.routes = router_1.RouterModule.forRoot(router);
