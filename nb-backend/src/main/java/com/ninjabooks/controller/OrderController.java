8d77b315b18c2fd1a6108f684ea2fecad1c6c51d
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2017-09-24 20:37:17 +0200

    Some refactoring in #order layer

---
+++ nb-backend/src/main/java/com/ninjabooks/controller/OrderController.java
@@ -0,0 +1,31 @@
+package com.ninjabooks.controller;
+
+import com.ninjabooks.error.order.OrderException;
+import com.ninjabooks.service.rest.order.OrderBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Piotr 'pitrecki' Nowak
+ * @since 1.0
+ */
+@RestController
+public class OrderController
+{
+    private final OrderBookService orderBookService;
+
+    @Autowired
+    public OrderController(OrderBookService orderBookService) {
+        this.orderBookService = orderBookService;
+    }
+
+
+    @ResponseStatus(value = HttpStatus.OK)
+    @RequestMapping(value = "/api/orderd/{userID}/", method = RequestMethod.POST)
+    public void orderBook(@PathVariable Long userID,
+                          @RequestParam(value = "bookID") Long bookID)
+        throws OrderException {
+        orderBookService.orderBook(userID, bookID);
+    }
+}
