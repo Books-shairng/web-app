9d897213a317a44c0a72b37b19b8f4bdc19cada0
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2017-10-09 12:08:42 +0200

    Migrate transaction into spring transaction type instead javax persistence.

--- nb-backend/src/main/java/com/ninjabooks/dao/db/DBQueueDao.java
+++ nb-backend/src/main/java/com/ninjabooks/dao/db/DBQueueDao.java
@@ -8,8 +8,8 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.stream.Stream;
--- nb-backend/src/main/java/com/ninjabooks/util/db/StreamFinder.java
+++ nb-backend/src/main/java/com/ninjabooks/util/db/StreamFinder.java
@@ -5,8 +5,7 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author Piotr 'pitrecki' Nowak
