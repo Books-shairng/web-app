68920381e7f20f8b9736323988149fdc98672953
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2017-10-13 21:40:10 +0200

    ????

--- nb-backend/src/integration-test/java/com/ninjabooks/service/rest/lend/BookRentalServiceImplIT.java
+++ nb-backend/src/integration-test/java/com/ninjabooks/service/rest/lend/BookRentalServiceImplIT.java
@@ -1,11 +1,11 @@
 package com.ninjabooks.service.rest.lend;
 
 import com.ninjabooks.config.IntegrationTest;
-import com.ninjabooks.dao.BookDao;
-import com.ninjabooks.dao.UserDao;
 import com.ninjabooks.domain.Book;
 import com.ninjabooks.domain.BookStatus;
 import com.ninjabooks.domain.Borrow;
+import com.ninjabooks.service.dao.borrow.BorrowService;
+import com.ninjabooks.service.dao.user.UserService;
 import com.ninjabooks.util.constants.DomainTestConstants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,8 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -34,23 +36,25 @@ public class BookRentalServiceImplIT
     private BookRentalService sut;
 
     @Autowired
-    private UserDao userDao;
+    private BorrowService borrowService;
 
     @Autowired
-    private BookDao bookDao;
+    private UserService userService;
 
     @Test
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     @Sql(value = "classpath:it_rent_import.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
     public void testRentBookShouldSucced() throws Exception {
         sut.rentBook(DomainTestConstants.ID, DomainTestConstants.DATA);
-        List<Borrow> borrows = userDao.getById(DomainTestConstants.ID).get().getBorrows();
+//        List<Borrow> borrows = userDao.getById(DomainTestConstants.ID).get().getBorrows();
+        Stream<Borrow> actual = borrowService.getAll();
 
         assertSoftly(softly -> {
-            assertThat(borrows).hasSize(EXPECTED_SIZE);
-            assertThat(borrows).extracting("book", Book.class)
+            assertThat(actual).hasSize(EXPECTED_SIZE);
+            assertThat(actual).extracting("book", Book.class)
                 .usingElementComparatorIgnoringFields(IGNORED_FILEDS)
                 .containsExactly(DomainTestConstants.BOOK);
-            assertThat(borrows).extracting("book", Book.class)
+            assertThat(actual).extracting("book", Book.class)
                 .extracting(Book::getStatus)
                 .isEqualTo(BookStatus.BORROWED);
         });
