6b94a1a29f55e7820a2cd471beebc11736219342
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2017-10-22 21:38:22 +0200

    Fix failing tests

--- nb-backend/src/test/java/com/ninjabooks/service/rest/borrow/returnb/BookReturnServiceImplTest.java
+++ nb-backend/src/test/java/com/ninjabooks/service/rest/borrow/returnb/BookReturnServiceImplTest.java
@@ -1,5 +1,7 @@
 package com.ninjabooks.service.rest.borrow.returnb;
 
+import com.ninjabooks.domain.Book;
+import com.ninjabooks.domain.Borrow;
 import com.ninjabooks.error.borrow.BorrowException;
 import com.ninjabooks.service.dao.history.HistoryService;
 import com.ninjabooks.service.rest.borrow.RentalHelper;
@@ -37,7 +39,7 @@ public class BookReturnServiceImplTest
     @Before
     public void setUp() throws Exception {
         this.sut = new BookReturnServiceImpl(historyServiceMock, rentalHelperMock);
-        when(rentalHelperMock.findBookByQRCode(anyString())).thenReturn(DomainTestConstants.BOOK_FULL);
+        when(rentalHelperMock.findBookByQRCode(anyString())).thenReturn(createFreshEntity());
     }
 
     @Test
@@ -65,11 +67,22 @@ public class BookReturnServiceImplTest
 
     @Test
     public void testReturnBookShouldSucceed() throws Exception {
-        when(rentalHelperMock.isBookBorrowed(DomainTestConstants.BOOK)).thenReturn(true);
+        when(rentalHelperMock.isBookBorrowed(any())).thenReturn(true);
+
         sut.returnBook(DomainTestConstants.DATA);
 
         verify(rentalHelperMock, atLeastOnce()).findBookByQRCode(anyString());
         verify(rentalHelperMock, atLeastOnce()).isBookBorrowed(any());
         verify(historyServiceMock, atLeastOnce()).add(any());
+    }
+
+    private Book createFreshEntity() {
+        Book book = new Book();
+        Borrow borrow = new Borrow();
+        borrow.setBook(book);
+        borrow.setUser(DomainTestConstants.USER);
+        book.setBorrow(borrow);
+
+        return book;
     }
 }
