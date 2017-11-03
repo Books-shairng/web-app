e23a47f916960d9ceab624d639385a6f908f8249
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2017-11-03 22:06:55 +0100

    Fetching comments service layer implemented

--- nb-backend/src/main/java/com/ninjabooks/service/rest/comment/CommentServiceImpl.java
+++ nb-backend/src/main/java/com/ninjabooks/service/rest/comment/CommentServiceImpl.java
@@ -1,17 +1,39 @@
 package com.ninjabooks.service.rest.comment;
 
+import com.ninjabooks.domain.Book;
 import com.ninjabooks.json.comment.CommentResponse;
+import com.ninjabooks.json.comment.CommentResponseFactory;
+import com.ninjabooks.service.dao.book.BookDaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author Piotr 'pitrecki' Nowak
  * @since 1.0
  */
+@Service
+@Transactional
 public class CommentServiceImpl implements CommentService
 {
+    private final BookDaoService bookDaoService;
+
+    @Autowired
+    public CommentServiceImpl(BookDaoService bookDaoService) {
+        this.bookDaoService = bookDaoService;
+    }
+
     @Override
     public Set<CommentResponse> fetchBookComments(String isbn) {
-        return null;
+        return bookDaoService.getByISBN(isbn)
+            .parallel()
+            .map(Book::getComments)
+            .flatMap(Collection::stream)
+            .map(CommentResponseFactory::makeCommentResponse)
+            .collect(Collectors.toSet());
     }
 }
