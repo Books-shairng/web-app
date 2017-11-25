fbf8a7cd3b58f115055dec075c0025b15a942fb8
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2017-11-25 12:43:07 +0100

    Add history dto object patten

---
+++ nb-backend/src/main/java/com/ninjabooks/dto/HistoryDto.java
@@ -0,0 +1,29 @@
+package com.ninjabooks.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.ninjabooks.util.converter.json.LocalDateDeserializer;
+import com.ninjabooks.util.converter.json.LocalDateSerializer;
+
+import java.time.LocalDate;
+
+/**
+ * @author Piotr 'pitrecki' Nowak
+ * @since 1.0
+ */
+public class HistoryDto extends BaseEntityDto
+{
+    private static final long serialVersionUID = -8941441076513470324L;
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonSerialize(using = LocalDateSerializer.class)
+    private LocalDate returnDate;
+
+    public LocalDate getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(LocalDate returnDate) {
+        this.returnDate = returnDate;
+    }
+}
