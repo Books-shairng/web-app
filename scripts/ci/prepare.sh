366cd5205b94fb9adb304d309010dacb8f566e09
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2018-02-07 19:58:49 +0100

    yarn install fix

--- scripts/ci/prepare.sh
+++ scripts/ci/prepare.sh
@@ -2,7 +2,7 @@
 
 mvn -f ./nb-backend/ clean
 npm install -g yarn
-npm install -g @angular/cli
-yarn install --cwd ./nb-frontend/
-yarn add global @angular/cli --cwd ./nb-frontend/
+
+cd nb-frontend/
+yarm install
 yarn add global codecov --cwd ./nb-frontend/
