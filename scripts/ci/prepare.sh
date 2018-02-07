d4bf1165fa16d0e2ff59fcfe53cb2674ed94d5c6
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2018-02-07 19:45:29 +0100

    @angular/cli

--- scripts/ci/prepare.sh
+++ scripts/ci/prepare.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+
 mvn -f ./nb-backend/ clean
 npm install -g yarn
+npm install -g @angular/cli
 yarn install --cwd ./nb-frontend/
 yarn add global @angular/cli --cwd ./nb-frontend/
 yarn add global codecov --cwd ./nb-frontend/
