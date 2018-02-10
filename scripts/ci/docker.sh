7c9bd8d23c7accbf462c53ac551e26527da3bd59
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2018-02-10 18:13:18 +0100

    Move local env, echo and array inside function body

--- scripts/ci/docker.sh
+++ scripts/ci/docker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-CURRENT_DOCKER_TAG_VERSION=$( < .version )
-echo "Current Docker tag version is: $CURRENT_DOCKER_TAG_VERSION"
-
-nb_services=(frontend backend)
-
 function build_docker_images() {
+	CURRENT_DOCKER_TAG_VERSION=$( < .version )
+	echo "Current Docker tag version is: $CURRENT_DOCKER_TAG_VERSION"
+
+	nb_services=(frontend backend)
+
     if [ "$TRAVIS_BRANCH" == "develop" ] || [ "$TRAVIS_BRANCH" == "master" ]; then
         for index in ${nb_services[*]}
         do
