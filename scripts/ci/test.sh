651938713a382a0035c315cb8efd3cddfd5bec94
Piotr 'pitrecki' Nowak <piotr_nowak@outlook.com>
2018-02-10 16:03:53 +0100

    Set sudo to false, implicit clean tests

--- scripts/ci/test.sh
+++ scripts/ci/test.sh
@@ -2,12 +2,12 @@
 
 function bk_ut() {
     echo "############# Run backend unit tests #############"
-    mvn -f ./nb-backend/ test -q -Dlog4j.configurationFile=null
+    mvn clean -f ./nb-backend/ test -q -Dlog4j.configurationFile=null
     echo "############# Stop backend unit tests #############"
 }
 
 function bk_it() {
     echo "############# Run backend integration tests #############"
-    mvn -f ./nb-backend/ cobertura:cobertura-integration-test -q -Dlog4j.configurationFile=null
-    echo "############# Run backend integration tests #############"
+    mvn clean -f ./nb-backend/ cobertura:cobertura-integration-test -q -Dlog4j.configurationFile=null
+    echo "############# Stop backend integration tests #############"
 }
